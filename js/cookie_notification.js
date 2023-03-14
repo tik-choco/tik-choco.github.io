e3a64621565fd3dec63cfd2dcdbbb0b1ce56af4d
Creator Network <120567699+creatornet@users.noreply.github.com>
2023-03-14 14:29:30 +0900

    add cookie policy

---
+++ js/cookie_notification.js
@@ -0,0 +1,3 @@
+
+
+document.write('<div class="cookie-consent"><div class="cookie-text">当サイトでは、利便性の改善・閲覧の追跡のため、Cookieを使用しています。サイトの閲覧を続行した場合、Cookieの使用に同意したことになります。Cookieの使用に関する詳細は「<a href="privacy">プライバシーポリシー</a>」をご覧ください。</div><div class="cookie-agree">OK</div></div>');
